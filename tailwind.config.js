84246c0892579643a44b0c43f362c128eed4c3d4
Wasim Mirzayar <wasim.mirzayar@gmail.com>
2026-03-12 16:48:12 +0430

    lkjsdf

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",        // همه layout و صفحات داخل app/
+        "./components/**/*.{js,ts,jsx,tsx}", // اگر کامپوننت دارید
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
