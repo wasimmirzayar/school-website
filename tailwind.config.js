27bbcd3fd3bd5b6fc66d49eb52980cef47d0c8d7
Wasim Mirzayar <wasim.mirzayar@gmail.com>
2026-03-14 11:45:28 +0430

    Fixed TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-        "./app/**/*.{js,ts,jsx,tsx}",
+        "./src/app/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {
-            fontFamily: {
-                inter: ["Inter", "sans-serif"],
-                vazir: ["Vazirmatn", "sans-serif"],
-            },
-        },
+        extend: {},
     },
     plugins: [],
 };
