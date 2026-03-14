63ae8189c7d5d317458d25e7b82e067c1da3576f
Wasim Mirzayar <wasim.mirzayar@gmail.com>
2026-03-14 09:56:42 +0430

    tailwind.config.js changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 export default {
     content: [
-        "./app/**/*.{js,ts,jsx,tsx}",       // همه layout و صفحات
-        "./components/**/*.{js,ts,jsx,tsx}", // همه کامپوننت‌ها
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./app/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         extend: {
